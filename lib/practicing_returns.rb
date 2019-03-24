9cc09ba37ec6ff0356a365d4cf35e07f5cf267ba
Patrick A <akolo@hotmail.com>
2019-03-24 18:59:45 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,5 +8,10 @@ def hello(array)
   collection
 end
 
+<<<<<<< HEAD
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+=======
+binding.pry
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+>>>>>>> c017207885d4a819c106fd6b2a6991ed23ecc6cd
